470995fc9572e0fda0e108bc67341e1bfb26fbdf
Nishant Sharma <nishant.1511977@gmail.com>
2026-05-18 23:13:39 +0530

    first upload

---
+++ app.py
@@ -0,0 +1,356 @@
+from flask import Flask, render_template, redirect, request, url_for,request
+from flask_socketio import SocketIO, join_room, leave_room
+from flask_login import LoginManager, login_user,login_required,logout_user,current_user
+from pymongo.errors import DuplicateKeyError
+import random
+from db import (
+    get_user,
+    save_user,
+    add_friend,
+    get_friends,
+    get_notifications,
+    save_notification,
+    save_message,
+    get_messages,
+    delete_friend,
+    mark_notifications_as_read,
+   
+    
+)
+import string
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secretkey'
+
+socketio = SocketIO(app, async_mode='threading')
+waiting_user = None#3#######
+online_users = set()
+user_sockets = {}
+login_manager = LoginManager()
+login_manager.login_view ="login"
+login_manager.init_app(app)
+
+
+
+
+
+@app.route("/")
+def index():
+
+    friends = []
+    notifications = []
+
+    if current_user.is_authenticated:
+
+        friends = get_friends(
+            current_user.username
+        )
+
+        notifications = get_notifications(
+            current_user.username
+        )
+
+    return render_template(
+        "index.html",
+        friends=friends,
+        notifications=notifications,
+        online_users=online_users
+    )
+@socketio.on('disconnect')
+def handle_disconnect():
+
+    username = user_sockets.get(request.sid)
+
+    if username:
+
+        online_users.discard(username)
+
+        del user_sockets[request.sid]
+        socketio.emit(
+    'user_offline',
+    {
+        'username': username
+    }
+)
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    message = ''
+
+    if request.method == 'POST':
+
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        if len(username) < 3:
+
+            message = "Username too short"
+
+        elif "@" not in email or "." not in email:
+
+            message = "Invalid email"
+
+        elif len(password) < 6:
+
+            message = "Password must be at least 6 characters"
+
+        else:
+
+            try:
+
+                save_user(
+                    username,
+                    email,
+                    password
+                )
+
+                return redirect(url_for('login'))
+
+            except DuplicateKeyError:
+
+                message = "User already exists!!"
+
+    return render_template(
+        'signup.html',
+        message=message
+    )
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    message = ''
+
+    if request.method == 'POST':
+
+        username = request.form.get('username')
+        password_input = request.form.get('password')
+
+        user = get_user(username)
+
+        if user and user.check_password(password_input):
+
+            login_user(user)
+
+            return redirect(url_for('index'))
+
+        else:
+            message = 'Invalid username or password'
+
+    return render_template(
+        'login.html',
+        message=message
+    )
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/chat')
+def chat():
+
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+
+        messages = get_messages(room)
+
+        return render_template(
+            'chat.html',
+            username=username,
+            room=room,
+            messages=messages
+        )
+
+    else:
+        return redirect(url_for('index'))
+
+
+@app.route('/pm/<friend_username>')
+@login_required
+
+def private_chat(friend_username):
+
+    sorted_users = sorted([
+        current_user.username,
+        friend_username
+    ])
+    mark_notifications_as_read(
+    current_user.username,
+    friend_username
+)
+
+    room = f"pm_{sorted_users[0]}_{sorted_users[1]}"
+
+    return redirect(
+        url_for(
+            'chat',
+            username=current_user.username,
+            room=room
+        )
+    )   
+
+@app.route('/random')
+def random_chat():
+
+    username = "Guest" + ''.join(random.choices(string.digits, k=4))
+
+    room = "global_random_room"
+
+    return redirect(
+        url_for(
+            'chat',
+            username=username,
+            room=room
+        )
+    )
+
+
+@app.route('/add_friend', methods=['POST'])
+@login_required
+def add_friend_route():
+
+    friend_username = request.form.get('friend')
+
+    add_friend(current_user.username, friend_username)
+
+    return redirect(url_for('index'))
+
+@app.route('/delete_friend/<friend>')
+@login_required
+def delete_friend_route(friend):
+
+    delete_friend(
+        current_user.username,
+        friend
+    )
+
+    return redirect(url_for('index'))
+
+
+
+
+
+@socketio.on('send_message')
+def handle_send_message_event(data):
+
+    # SAVE ONLY PM MESSAGES
+    if data['room'].startswith("pm_"):
+
+        save_message(
+
+            data['room'],
+            data['message'],
+            data['username'],
+            data.get('image')
+
+        )
+
+    # SEND MESSAGE TO ROOM
+    socketio.emit(
+
+        'receive_message',
+
+        {
+            'username': data['username'],
+            'message': data['message'],
+            'image': data.get('image')
+        },
+
+        room=data['room']
+
+    )
+
+    # PM NOTIFICATIONS ONLY
+    if data['room'].startswith("pm_"):
+
+        users = data['room'].replace(
+            "pm_",
+            ""
+        ).split("_")
+
+        receiver = None
+
+        for user in users:
+
+            if user != data['username']:
+                receiver = user
+
+        save_notification(
+            receiver,
+            data['username']
+        )
+
+        socketio.emit(
+
+            'new_notification',
+
+            {
+                'sender': data['username']
+            },
+
+            room=f"user_{receiver}"
+
+        )
+@socketio.on('join_room')
+def handle_join_room_event(data):
+
+    join_room(data['room'])
+
+    socketio.emit(
+        'join_room_announcement',
+        data,
+        room=data['room'],
+        include_self=False
+    )
+
+
+@socketio.on('leave_room')
+def handle_leave_room_event(data):
+
+    leave_room(data['room'])
+
+    socketio.emit(
+        'leave_room_announcement',
+        data,
+        room=data['room'],
+        include_self=False
+    )
+
+
+@login_manager.user_loader
+def load_user(username):
+
+    return get_user(username)
+@socketio.on('join_personal_room')
+def handle_join_personal_room(data):
+
+    username = data['username']
+
+    online_users.add(username)
+
+    user_sockets[request.sid] = username
+
+    join_room(f"user_{username}")
+    socketio.emit(
+    'user_online',
+    {
+        'username': username
+    }
+)
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
---
+++ db.py
@@ -0,0 +1,196 @@
+from datetime import datetime
+
+from bson import ObjectId
+from pymongo import MongoClient
+from pymongo import MongoClient, DESCENDING
+from werkzeug.security import generate_password_hash
+
+from user import User
+import os
+from pymongo import MongoClient
+
+MONGO_URI = os.environ.get("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+
+client = MongoClient("your_mongodb_uri_here")
+client = MongoClient("mongodb+srv://test:test@chatapp-q7wkc.mongodb.net/test?retryWrites=true&w=majority")
+
+chat_db = client.get_database("ChatDB")
+users_collection = chat_db.get_collection("users")
+rooms_collection = chat_db.get_collection("rooms")
+room_members_collection = chat_db.get_collection("room_members")
+messages_collection = chat_db.get_collection("messages")
+friends_collection = chat_db.get_collection("friends")
+notifications_collection = chat_db.get_collection("notifications")
+def save_user(username, email, password):
+    password_hash = generate_password_hash(password)
+    users_collection.insert_one({'_id': username, 'email': email, 'password': password_hash})
+
+
+def get_user(username):
+    user_data = users_collection.find_one({'_id': username})
+    return User(user_data['_id'], user_data['email'], user_data['password']) if user_data else None
+
+
+def save_room(room_name, created_by):
+    room_id = rooms_collection.insert_one(
+        {'name': room_name, 'created_by': created_by, 'created_at': datetime.now()}).inserted_id
+    add_room_member(room_id, room_name, created_by, created_by, is_room_admin=True)
+    return room_id
+
+
+def update_room(room_id, room_name):
+    rooms_collection.update_one({'_id': ObjectId(room_id)}, {'$set': {'name': room_name}})
+    room_members_collection.update_many({'_id.room_id': ObjectId(room_id)}, {'$set': {'room_name': room_name}})
+
+
+def get_room(room_id):
+    return rooms_collection.find_one({'_id': ObjectId(room_id)})
+
+
+def add_room_member(room_id, room_name, username, added_by, is_room_admin=False):
+    room_members_collection.insert_one(
+        {'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name, 'added_by': added_by,
+         'added_at': datetime.now(), 'is_room_admin': is_room_admin})
+
+
+def add_room_members(room_id, room_name, usernames, added_by):
+    room_members_collection.insert_many(
+        [{'_id': {'room_id': ObjectId(room_id), 'username': username}, 'room_name': room_name, 'added_by': added_by,
+          'added_at': datetime.now(), 'is_room_admin': False} for username in usernames])
+
+
+def remove_room_members(room_id, usernames):
+    room_members_collection.delete_many(
+        {'_id': {'$in': [{'room_id': ObjectId(room_id), 'username': username} for username in usernames]}})
+
+
+def get_room_members(room_id):
+    return list(room_members_collection.find({'_id.room_id': ObjectId(room_id)}))
+
+
+def get_rooms_for_user(username):
+    return list(room_members_collection.find({'_id.username': username}))
+
+
+def is_room_member(room_id, username):
+    return room_members_collection.count_documents({'_id': {'room_id': ObjectId(room_id), 'username': username}})
+
+
+def is_room_admin(room_id, username):
+    return room_members_collection.count_documents(
+        {'_id': {'room_id': ObjectId(room_id), 'username': username}, 'is_room_admin': True})
+
+
+def save_message(
+    room_id,
+    text,
+    sender,
+    image=None,
+    
+):
+
+    messages_collection.insert_one({
+
+        'room_id': room_id,
+        'text': text,
+        'sender': sender,
+        'image': image,
+        'created_at': datetime.now()
+
+    })
+
+
+def get_messages(room_id, page=0):
+    offset = page * MESSAGE_FETCH_LIMIT
+    messages = list(
+        messages_collection.find({'room_id': room_id}).sort('_id', DESCENDING).limit(MESSAGE_FETCH_LIMIT).skip(offset))
+    for message in messages:
+        message['created_at'] = message['created_at'].strftime("%d %b, %H:%M")
+    return messages[::-1]
+def add_friend(user, friend):
+
+    friends_collection.insert_one({
+        "user": user,
+        "friend": friend
+    })
+def get_friends(user):
+
+    return list(
+        friends_collection.find({
+            "user": user
+        })
+    )    
+# Save a private message
+def save_private_message(sender, receiver, text):
+    conversation_id = get_conversation_id(sender, receiver)
+    messages_collection.insert_one({
+        'conversation_id': conversation_id,
+        'text': text,
+        'sender': sender,
+        'receiver': receiver,
+        'created_at': datetime.now()
+    })
+
+# Generate a consistent conversation ID
+def get_conversation_id(user1, user2):
+    # Ensure same ID regardless of order
+    return ":".join(sorted([user1, user2]))
+
+# Fetch private messages
+MESSAGE_FETCH_LIMIT = 20
+
+def get_private_messages(user1, user2, page=0):
+    conversation_id = get_conversation_id(user1, user2)
+    offset = page * MESSAGE_FETCH_LIMIT
+    messages = list(
+        messages_collection.find({'conversation_id': conversation_id})
+        .sort('_id', DESCENDING)
+        .limit(MESSAGE_FETCH_LIMIT)
+        .skip(offset)
+    )
+    for message in messages:
+        message['created_at'] = message['created_at'].strftime("%d %b, %H:%M")
+    return messages[::-1]
+
+def save_notification(user, sender):
+
+    notifications_collection.insert_one({
+        "user": user,
+        "sender": sender,
+        "read": False
+    })
+
+
+def get_notifications(user):
+
+    return list(
+        notifications_collection.find({
+            "user": user,
+            "read": False
+        })
+    )
+
+
+def mark_notifications_as_read(user, sender):
+
+    notifications_collection.update_many(
+        {
+            "user": user,
+            "sender": sender
+        },
+        {
+            "$set": {
+                "read": True
+            }
+        }
+    )
+
+
+def delete_friend(user, friend):
+
+    friends_collection.delete_one({
+        "user": user,
+        "friend": friend
+    })
