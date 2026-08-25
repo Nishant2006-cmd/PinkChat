ec2c6f34be6b5311115d77394913f300f4bd541c
Nishant Sharma <nishant.1511977@gmail.com>
2026-08-25 06:55:01 +0530

    update

--- app.py
+++ app.py
@@ -336,10 +336,10 @@ def load_user(username):
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5001))
-    socketio.run(
-        app,
-        host="0.0.0.0",
-        port=port,
-        debug=False
-    )
+    port = int(os.environ.get("PORT", 5001))
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
